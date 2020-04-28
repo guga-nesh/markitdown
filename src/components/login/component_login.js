2fae49d2bdd5d5e1c926f9979c612e9eb38727f4
Prithivi Raj <ptvrajsk@gmail.com>
2020-04-28 20:14:15 +0800

    Removing Warnings

--- src/components/login/component_login.js
+++ src/components/login/component_login.js
@@ -37,20 +37,20 @@ class LoginComponent extends React.Component {
         firebase.auth().signInWithPopup(provider).then(function(result) {
             console.log("SUCCESS")
             // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
+            // var token = result.credential.accessToken;
             // The signed-in user info.
-            var user = result.user;
+            // var user = result.user;
             // ...
           }).catch(function(error) {
             console.log("NOT SUCCESS")
             // Handle Errors here.
-            var errorCode = error.code;
+            // var errorCode = error.code;
             var errorMessage = error.message;
             console.log(errorMessage);
             // The email of the user's account used.
-            var email = error.email;
+            // var email = error.email;
             // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
+            // var credential = error.credential;
             // ...
           });
     }
